8ddb3cd5b9ed5869018876fb6e9af969b8b475bd
NotMichaelChen <mzc12345@gmail.com>
2016-04-28 22:11:16 -0400

    Updated .gitignore and created ImageDetector.cs

---
+++ ImageProcessor/ImageDetector.cs
@@ -0,0 +1,26 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: guest1
+ * Date: 4/28/2016
+ * Time: 10:02 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Drawing;
+
+namespace Popn_image_processor.ImageProcessor
+{
+    /// <summary>
+    /// Extracts notes from a pop'n music image chart
+    /// </summary>
+    public class ImageDetector
+    {
+        private Bitmap chart;
+        
+        public ImageDetector(string imagefile)
+        {
+            chart = new Bitmap(imagefile);
+        }
+    }
+}
