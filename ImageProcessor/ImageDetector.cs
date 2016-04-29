9f12e5393dcf54a8f97302269126be7eff9b3ba1
NotMichaelChen <mzc12345@gmail.com>
2016-04-28 22:43:57 -0400

    Added ImageDetector.GetNotes

--- ImageProcessor/ImageDetector.cs
+++ ImageProcessor/ImageDetector.cs
@@ -1,12 +1,9 @@
 ﻿/*
- * Created by SharpDevelop.
  * User: guest1
  * Date: 4/28/2016
- * Time: 10:02 PM
- * 
- * To change this template use Tools | Options | Coding | Edit Standard Headers.
  */
 using System;
+using System.Collections.Generic;
 using System.Drawing;
 
 namespace Popn_image_processor.ImageProcessor
@@ -17,10 +14,26 @@ namespace Popn_image_processor.ImageProcessor
     public class ImageDetector
     {
         private Bitmap chart;
+        //Uses bit-mask of length 9 to store notes in 1/4 intervals
+        private List<int> notes;
         
+        /// <summary>
+        /// Constructs an object to extract notes from a pop'n image
+        /// </summary>
+        /// <param name="imagefile">Path of the image file</param>
         public ImageDetector(string imagefile)
         {
             chart = new Bitmap(imagefile);
+            notes = new List<int>();
+        }
+        
+        /// <summary>
+        /// Gets all of the notes in the image file
+        /// </summary>
+        /// <returns>An array of bitmasks in 1/4 intervals</returns>
+        public int[] GetNotes()
+        {
+            throw new NotImplementedException();
         }
     }
 }
