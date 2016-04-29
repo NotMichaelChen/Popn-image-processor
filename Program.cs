5d4f9987f646c1696dfd401e464bf0738d4a18db
NotMichaelChen <mzc12345@gmail.com>
2016-04-28 23:25:01 -0400

    Imported ImageProcessor in Program.cs

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@
  * Date: 4/28/2016
  */
 using System;
+using Popn_image_processor.ImageProcessor;
 
 namespace Popn_image_processor
 {
@@ -11,6 +12,8 @@ namespace Popn_image_processor
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            
+            ImageDetector id = new ImageDetector("C:\\Users\\guest1\\Downloads\\parapara[hyper].png");
             
             Console.Write("Press any key to continue . . . ");
             Console.ReadKey(true);
