6a8474fd17f813c90c40531253e1226c8ea1bea7
NotMichaelChen <mzc12345@gmail.com>
2016-04-28 21:56:06 -0400

    Added .gitignore and initial Program.cs

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: guest1
+ * Date: 4/28/2016
+ * Time: 9:38 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Popn_image_processor
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
